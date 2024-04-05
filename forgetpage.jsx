5ba131899cf2c953d5866985678c594ce3c3c1ca
Anamikaanoop <165902414+Anamikaanoop@users.noreply.github.com>
2024-04-05 16:47:57 +0530

    Add files via upload

---
+++ forgetpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../components/home';
+import Forget from '../components/forget ';
+
+const Forgetpage = () => {
+
+  return (
+    <Forget/>
+  );
+};
+
+export default Forgetpage;
--- home.jsx
+++ home.jsx
@@ -1,33 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import Home from '../components/home';
 
-const Home = () => {
-  const dispatch = useDispatch()
-  const navigator = useNavigate()
-  const logout = () => {
-    dispatch({ type: 'SET_USER', payload: 'user not found' }); 
-    navigator('/login')
-  }
-  const user = useSelector((state) => state.user.user);
+const HomePage = () => {
 
   return (
-
-    <center>
-    <div style={{backgroundImage:`url(/images2.jpg)`,backgroundSize:"cover",backgroundRepeat:"no-repeat",height:"800px",width:"1500px"}}>
-          <div style={{fontFamily:'Times New Roman',fontSize:'30px'}} ><h1 >Home</h1></div>
-   
-    <div style={{borderRadius:'100px',border:'2px solid black',borderColor:'maroon',borderColor:'black',borderWidth:'thick',gap:'10px',width:'300px',padding:'60px',height:'400px'}}><h2>Welcome, {user}!</h2>
-    <br></br>
-    <p>This is your home page.</p>
-    <br></br><br></br><br></br><br></br><br></br><br></br>
-    <button style={{width:'200px',height:'50px',borderBlockColor:'pink',fontSize:"20px"}}onClick={logout}>logout</button>
-    </div>
-    
-    
-  </div>
-  </center>
+    <Home />
   );
 };
-export default Home;
+
+export default HomePage;
